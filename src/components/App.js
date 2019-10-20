82df771f2eb62163459977dc424a1901515f7350
xomolya <dp131192pka@gmail.com>
2019-10-20 15:48:04 +0300

    new

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,33 @@
-import React from 'react';
-import Footer from './Footer';
-import Header from './Header';
+import React, { useState } from 'react';
+
+
+const names = ['vasa', 'pitya', 'alex', 'an'];
+
+
+const render = names.reduce((ak, name, i) => {
+  if (name !== 'pitya'){
+    ak.push(<p key={i}>{name}</p>)
+    return ak;
+  }
+  return ak;
+}, []);
+
+ console.log(render);
 
 function App() {
+
+  const [res, setRes] = useState('hello');
+  
+const show = () => {
+  setRes(res + ' world');
+}
+
+
   return (
-    <>
-      <Header />
-      <Footer />
-    </>
+    <div id="app">
+      <h2 onClick={show}>Hello app</h2>
+      {res}
+    </div>
   );
 }
 
