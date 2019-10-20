77cbc6142a7e7d285ea18bbf97ce0c19e5dcf409
xomolya <dp131192pka@gmail.com>
2019-10-20 13:36:17 +0300

    project-start

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <footer>
+            dev 2019
+        </footer>
+    );
+}
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <header>
+            <ul>
+                <li>Home</li>
+                <li>About us</li>
+                <li>Contacts</li>
+            </ul>
+        </header>
+    );
+}
+
+export default Header;
