07e0aa035254c19bc6859986923707e695e67b2f
xomolya <dp131192pka@gmail.com>
2019-10-25 21:29:18 +0300

    v2

---
+++ src/components/pages/Home/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Home = (props) => {
+    const {title, numberPage} = props;
+    return (
+        <div className='Home'>
+            <h1>{title}</h1>
+            <h2>{numberPage}</h2>
+        </div>
+    )
+}
+
+
+
+export default Home;
